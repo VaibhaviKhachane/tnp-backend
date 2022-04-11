0f905c02bd4dbac9050007ed5a06a4b809a2af4d
VaibhaviKhachane <vaibhavikhachane30@gmail.com>
2022-04-11 15:49:13 +0530

    Changed the port number

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.use('/api/logcmpny',logcmpny);
 app.use('/api/drive', drive);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, ()=> console.log(`Listening on port ${port}...`));
 
